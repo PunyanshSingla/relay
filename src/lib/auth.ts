248dd8126658ec07ee862b6c597ed747753cc22c
PunyanshSingla <punyanshcoder@gmail.com>
2026-06-13 17:35:54 +0530

    feat: integrate better-auth with Prisma and PostgreSQL for user authentication

---
+++ src/lib/auth.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { dash } from "@better-auth/infra";
+
+const client = new PrismaClient();
+
+export const auth = betterAuth({
+  database: prismaAdapter(client, { provider: "postgresql" }),
+  baseURL: "http://localhost:3000/",
+  emailAndPassword: { enabled: true },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+  },
+  plugins: [dash()]
+});
